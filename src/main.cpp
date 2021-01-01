0847b89c6d3e7505cd45de90c25d4536d6b5076c
ArBe <argon-beryllium@protonmail.com>
2021-01-01 17:10:51 +0100

    added splash screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,39 @@
 #include <bj/shitrndr.h>
 using namespace bj;
 
-struct GameScene : public Scene
+struct SplashScene : public Scene
 {
 	void onStart() override
 	{
 		UI::loadFont("res/ProggyTiny.ttf", .05);
 		UI::loadFont("res/Ludum-Dairy-0.2.0.ttf", .07);
 		UI::loadFont("res/KingthingsExtortion-YMAa.ttf", .075);
-		shitrndr::bg_col = {2,2,22,255};
-		std::cout << shitrndr::WindowProps::getWidth() << '\n';
+		instantiate()->addComponent(new SpriteRen(getObj(0), "res/logo.png"));
+		getObj(0)->transform.scl.y = 0;
+		cam.pos = {.5,.5};
+		cam.scale = 2;
+	}
+	void onLoad() override { shitrndr::bg_col = {3,0,4,255}; }
+	void onRenderFG(float d, float t) override
+	{
+		constexpr float start = 1, speed = 4.5;
+		if(t>start && t-start<M_PI/(speed*1.8))
+		{
+			getObj(0)->transform.scl.y = std::sin((t-start)*speed);
+			getObj(0)->transform.pos.y = .5-getObj(0)->transform.scl.y/2;
+		}
+		if(t<M_PI) SDL_SetRenderDrawColor(shitrndr::ren, 3, 0, 4, (Uint8)((1-std::sin(t))*255));
+		UI::renderStaticText(.5, .8, "a game by ArBe", {UI::CENTRED});
+		shitrndr::FillRect(shitrndr::WindowProps::getSizeRect());
+
+		if(t>4.5) SceneManager::setActiveScene(1);
+	}
+};
+struct GameScene : public Scene
+{
+	void onStart() override
+	{
+		cam.pos = {.5,.5};
 		instantiate()->addComponent(new BasicRen(getObj(0)));
 		getObj(0)->addComponent(new Behaviour(getObj(0), [](Behaviour* self, const ecs::Event& e)
 					{
@@ -23,20 +47,17 @@ struct GameScene : public Scene
 						UI::renderStaticText(.5, .7, "POG", {UI::CENTRED, 2});
 					}));
 	}
-	void onRenderBG(float d, float t) override
-	{
-		SDL_SetRenderDrawColor(shitrndr::ren, shitrndr::bg_col.r, shitrndr::bg_col.g, shitrndr::bg_col.b, shitrndr::bg_col.a);
-		shitrndr::FillRect(shitrndr::WindowProps::getSizeRect());
-	}
+	void onLoad() override { shitrndr::bg_col = {2,2,22,255}; }
 };
 
 int BJ_MULP_MAIN()
 {
 	initParams ip;
-	ip.nscenes = 1;
-	ip.scenes = new Scene*[1]{new GameScene()};
+	ip.nscenes = 2;
+	ip.scenes = new Scene*[2]{new SplashScene(), new GameScene()};
 
 	Game::init(ip);
 	shitrndr::WindowProps::setPixScale(3);
+	SDL_SetRenderDrawBlendMode(shitrndr::ren, SDL_BLENDMODE_BLEND);
 	Game::start();
 }
